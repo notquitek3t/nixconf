5cf2db7374d90a6aa4882ab57551cd64507c10bb
k3t <k3t@notquitek3t.xyz>
2026-03-12 07:42:21 -0600

    zsh car patches

--- home/car.nix
+++ home/car.nix
@@ -3,10 +3,10 @@
     ./common.nix
   ];
   home.file = {
-    ".profile".text = ''
-    bash /home/k3t/.car/init.sh
-    '';
+    #".profile".text = ''
+    #bash /home/k3t/.car/init.sh
+    #'';
     ".car/init.sh".source = ../hosts/pi-car/scripts/init.sh;
   };
-  #programs.zsh.loginExtra = "bash /home/k3t/.car/init.sh";
+  programs.zsh.loginExtra = "bash /home/k3t/.car/init.sh";
 }
