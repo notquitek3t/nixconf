a2a7bbc12f5184c6f8c3600da6a2cc77cc258962
k3t <k3t@notquitek3t.xyz>
2026-03-18 07:40:08 -0600

    enable kwallet

--- modules/desktop-common.nix
+++ modules/desktop-common.nix
@@ -197,6 +197,8 @@
   };
   environment.etc."firefox/policies/policies.json".target = "librewolf/policies/policies.json";
 
+  security.pam.services.k3t.kwallet.enable = true;
+  security.pam.services.k3t.kwallet.forceRun = true;
   users.users.k3t = {
     packages = with pkgs; [
       joplin-desktop
